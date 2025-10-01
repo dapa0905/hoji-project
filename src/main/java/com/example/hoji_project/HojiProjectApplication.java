34bc343fcdce55b8ce543cea3f8d25c674eb31ac
KANGMO KOO <kookangmo0905@gmail.com>
2025-10-01 16:11:38 +0900

    251001_firstCommit

---
+++ src/main/java/com/example/hoji_project/HojiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoji_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HojiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HojiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hoji_project/HojiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoji_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HojiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
